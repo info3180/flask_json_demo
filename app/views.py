ed4ba0c00c9e11699bcb52ad5e76477772c4480a
David Bain <david@alteroo.com>
2015-02-16 16:08:57 -0500

    initial commit

---
+++ app/views.py
@@ -0,0 +1,43 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+
+This file creates your application.
+"""
+
+from app import app
+from flask import render_template, request, redirect, url_for
+from flask import jsonify,session
+
+username = "bob"
+email = "bob@example.com"
+id = "3424324"
+
+###
+# Routing for your application.
+###
+@app.route('/_get_current_user')
+def get_current_user():
+    return jsonify(username=username,
+                   email=email,
+                   id=id)
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="8888")
